ba078aa90229eb2a1e391818d2c71120f757b4e4
fuqijia <60819777+fuqijia@users.noreply.github.com>
2020-03-13 23:56:33 +0800

    废弃了python方案

--- InfectStatistic/ppp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class data(models.Model):
-    id=models.AutoField,
-    item=models.AutoField,
--- InfectStatistic/ppp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from ppp import models
-# Create your views here.
-from django.shortcuts import HttpResponse
-
-def index(request):
-   # return HttpResponse("hello world!")
-   data_list=models.data.objects.all()
-   return render(request,"index.html",{"data":data_list})
